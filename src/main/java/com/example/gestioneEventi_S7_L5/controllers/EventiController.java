d0ac42d126276ac976134b361b1b8750cf52e73e
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-09-20 12:41:41 +0200

    create EventiService and controller

---
+++ src/main/java/com/example/gestioneEventi_S7_L5/controllers/EventiController.java
@@ -0,0 +1,71 @@
+package com.example.gestioneEventi_S7_L5.controllers;
+
+import com.example.gestioneEventi_S7_L5.entities.Evento;
+import com.example.gestioneEventi_S7_L5.entities.Utente;
+import com.example.gestioneEventi_S7_L5.exceptions.BadRequestException;
+import com.example.gestioneEventi_S7_L5.payloads.EventoDTO;
+import com.example.gestioneEventi_S7_L5.services.EventiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/eventi")
+public class EventiController {
+
+    @Autowired
+    private EventiService eventiService;
+
+
+    @GetMapping()
+    public Page<Evento> getAll(@RequestParam(defaultValue = "0") int page,
+                               @RequestParam(defaultValue = "10") int size,
+                               @RequestParam(defaultValue = "id") String sortBy) {
+        return this.eventiService.findAll(page, size, sortBy);
+    }
+
+    @GetMapping("/{eventoId}")
+    public Evento getById(@PathVariable UUID eventoId) {
+        return eventiService.findById(eventoId);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN','ORGANIZZATORE_EVENTI')") // Solo gli admin possono modificare altri utenti
+    @ResponseStatus(HttpStatus.CREATED)
+    public Evento saveEvento(@RequestBody @Validated EventoDTO eventoDTO, BindingResult validationResult) {
+
+        if (validationResult.hasErrors()) {
+            // Se ci sono stati errori lanciamo un'eccezione custom
+            String messages = validationResult.getAllErrors().stream()
+                    .map(objectError -> objectError.getDefaultMessage())
+                    .collect(Collectors.joining(". "));
+
+            throw new BadRequestException("Ci sono stati errori nel payload. " + messages);
+        } else {
+            // Se non ci sono stati salviamo l'utente
+            return eventiService.save(eventoDTO);
+        }
+    }
+
+    @PutMapping("/{eventoId}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','ORGANIZZATORE_EVENTI')")
+    public Evento findByIdAndUpdate(@PathVariable UUID eventoId, @RequestBody @Validated EventoDTO updatedEventoDTO) {
+        return eventiService.findByIdAndUpdate(eventoId, updatedEventoDTO);
+    }
+
+    @DeleteMapping("/{eventoId}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','ORGANIZZATORE_EVENTI')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void findByIdAndDelete(@PathVariable UUID eventoId) {
+        eventiService.findByIdAndDelete(eventoId);
+    }
+
+
+}
---
+++ src/main/java/com/example/gestioneEventi_S7_L5/services/EventiService.java
@@ -0,0 +1,96 @@
+package com.example.gestioneEventi_S7_L5.services;
+
+import com.example.gestioneEventi_S7_L5.entities.Evento;
+import com.example.gestioneEventi_S7_L5.entities.Utente;
+import com.example.gestioneEventi_S7_L5.exceptions.BadRequestException;
+import com.example.gestioneEventi_S7_L5.exceptions.NotFoundException;
+import com.example.gestioneEventi_S7_L5.payloads.EventoDTO;
+import com.example.gestioneEventi_S7_L5.repositories.EventiRepository;
+import com.example.gestioneEventi_S7_L5.repositories.UtentiRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Service
+public class EventiService {
+    @Autowired
+    private EventiRepository eventiRepository;
+
+    @Autowired
+    private  UtentiRepository utentiRepository;
+
+    public Page<Evento> findAll(int page, int size, String sortBy){
+        if(page > 100) page = 100;
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return this.eventiRepository.findAll(pageable);
+    }
+
+    public Evento save(EventoDTO eventoDTO){
+
+        UUID uuidUtente;
+        LocalDate dataEvento = null;
+        try {
+            dataEvento= LocalDate.parse(eventoDTO.dataEvento());
+        }catch (Exception e){
+            throw new BadRequestException("Errore: il formato data inserito non è corretto. inserire un formato data (yyyy-mm-dd)");
+        }
+
+        try {
+            uuidUtente=  UUID.fromString(eventoDTO.organizzatore());
+        }catch (Exception e){
+            throw new BadRequestException("L'id inserito non è valido! Necessario inserire un ID di Tipo UUID");
+        }
+        Utente organizzatore = utentiRepository.findById(uuidUtente).orElseThrow(() ->  new NotFoundException(uuidUtente));
+
+
+        Evento evento = new Evento(eventoDTO.titolo(),eventoDTO.descrizione(),dataEvento,eventoDTO.nrPostiDisponibili(),organizzatore);
+
+        return eventiRepository.save(evento);
+
+    }
+
+    public Evento findById(UUID eventoId){
+        return this.eventiRepository.findById(eventoId).orElseThrow(() -> new NotFoundException(eventoId));
+    }
+
+    public Evento findByIdAndUpdate(UUID  eventoId, EventoDTO updatedEventoDTO){
+        Evento found = findById(eventoId);
+
+
+        LocalDate dataEvento = null;
+        try {
+            dataEvento= LocalDate.parse(updatedEventoDTO.dataEvento());
+        }catch (Exception e){
+            throw new BadRequestException("Errore: il formato data inserito non è corretto. inserire un formato data (yyyy-mm-dd)");
+        }
+
+        UUID uuidUtente;
+        try {
+            uuidUtente=  UUID.fromString(updatedEventoDTO.organizzatore());
+        }catch (Exception e){
+            throw new BadRequestException("L'id inserito non è valido! Necessario inserire un ID di Tipo UUID");
+        }
+        Utente organizzatore = utentiRepository.findById(uuidUtente).orElseThrow(() ->  new NotFoundException(uuidUtente));
+
+
+        found.setTitolo(updatedEventoDTO.titolo());
+        found.setDescrizione(updatedEventoDTO.descrizione());
+        found.setDataEvento(dataEvento);
+        found.setNrPostiDisponibili(updatedEventoDTO.nrPostiDisponibili());
+        found.setOrganizzatore(organizzatore);
+
+        return this.eventiRepository.save(found);
+    }
+
+    public void findByIdAndDelete(UUID  viaggioId){
+        Evento found = findById(viaggioId);
+        this.eventiRepository.delete(found);
+    }
+}
