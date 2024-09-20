5b56a33f22db88151b80896c8a98b84bad46cb4a
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-09-20 10:47:02 +0200

    Initial commit

---
+++ src/main/java/com/example/gestioneEventi_S7_L5/GestioneEventiS7L5Application.java
@@ -0,0 +1,13 @@
+package com.example.gestioneEventi_S7_L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneEventiS7L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneEventiS7L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestioneEventi_S7_L5/GestioneEventiS7L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestioneEventi_S7_L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiS7L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
